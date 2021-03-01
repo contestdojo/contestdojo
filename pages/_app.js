329a0e39c8f221144a1eb1f22499d5d0bdecfd86
Oliver Ni <oliver.ni@gmail.com>
2021-03-01 11:21:24 -0800

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { Center, ChakraProvider, Spinner } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
@@ -83,6 +84,15 @@ const App = ({ Component, pageProps }) => {
     return (
         <>
             <DefaultSeo title="SMT" description="SMT Dashboard" />
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <ErrorBoundary>
                 <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense>
                     <NoSSR>
