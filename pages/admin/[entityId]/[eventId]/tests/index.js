9cdf2fa8d797bed7add0c331fc3d348f0a4a6825
Oliver Ni <oliver.ni@gmail.com>
2021-04-16 15:37:53 -0700

    Add delete button

--- pages/admin/[entityId]/[eventId]/tests/index.js
+++ pages/admin/[entityId]/[eventId]/tests/index.js
@@ -1,19 +1,18 @@
-import AddTestForm from "~/components/forms/AddTestForm";
 import { Button, IconButton } from "@chakra-ui/button";
 import { Box, Heading, HStack, Stack, Text } from "@chakra-ui/layout";
+import { Tag } from "@chakra-ui/tag";
 import { Tooltip } from "@chakra-ui/tooltip";
 import dayjs from "dayjs";
 import firebase from "firebase";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { HiClipboardCheck, HiPencilAlt, HiTable } from "react-icons/hi";
+import { HiClipboardCheck, HiPencilAlt, HiTable, HiTrash } from "react-icons/hi";
 import { useFirestoreCollectionData } from "reactfire";
 import Card from "~/components/Card";
 import { useDialog } from "~/components/contexts/DialogProvider";
 import { useEvent } from "~/components/contexts/EventProvider";
+import AddTestForm from "~/components/forms/AddTestForm";
 import { toDict, useFormState, useTime } from "~/helpers/utils";
-import { Tag } from "@chakra-ui/tag";
 
 const TooltipLink = ({ label, href, children }) => (
     <Tooltip label={label}>
@@ -25,13 +24,46 @@ const TooltipLink = ({ label, href, children }) => (
     </Tooltip>
 );
 
-const TestCard = ({ id, type, name, team, duration, openTime: rawOpenTime, closeTime: rawCloseTime, time, onOpen }) => {
+const TestCard = ({
+    id,
+    type,
+    name,
+    team,
+    duration,
+    openTime: rawOpenTime,
+    closeTime: rawCloseTime,
+    time,
+    onOpen,
+    onDelete,
+}) => {
     const router = useRouter();
     const { entityId, eventId } = router.query;
 
     const openTime = rawOpenTime && dayjs(rawOpenTime?.toDate());
     const closeTime = rawCloseTime && dayjs(rawCloseTime?.toDate());
     const open = time.isAfter(openTime) && time.isBefore(closeTime);
+
+    const [openDialog] = useDialog();
+
+    const handleOpen = () => {
+        openDialog({
+            type: "confirm",
+            title: "Are you sure?",
+            description: `This will open the test for all students for a window of ${
+                duration / 60 + 10
+            } minutes. Confirm?`,
+            onConfirm: onOpen,
+        });
+    };
+
+    const handleDelete = () => {
+        openDialog({
+            type: "confirm",
+            title: "Are you sure?",
+            description: "This action cannot be undone.",
+            onConfirm: onDelete,
+        });
+    };
 
     return (
         <Card as={HStack} p={4} key={id}>
@@ -65,9 +97,11 @@ const TestCard = ({ id, type, name, team, duration, openTime: rawOpenTime, close
                 <IconButton as="a" icon={<HiTable />} />
             </TooltipLink>
 
-            <Button colorScheme="blue" onClick={onOpen} minW={150} disabled={open}>
+            <Button colorScheme="blue" onClick={handleOpen} minW={150} disabled={open}>
                 {open ? "Open" : openTime ? "Reopen Test" : "Open Test"}
             </Button>
+
+            <IconButton icon={<HiTrash />} colorScheme="red" onClick={handleDelete} />
         </Card>
     );
 };
@@ -79,18 +113,20 @@ const TestsTab = () => {
     const testsRef = eventRef.collection("tests");
     const { data: tests } = useFirestoreCollectionData(testsRef, { idField: "id" });
     let testsById = tests.reduce(toDict, {});
-
     const [formState, wrapAction] = useFormState();
-    const [openDialog] = useDialog();
 
-    const handleOpenTest = openTest => async () => {
+    const handleOpenTest = test => async () => {
         const now = new Date();
-        await testsRef.doc(openTest.id).update({
+        await testsRef.doc(test.id).update({
             openTime: firebase.firestore.Timestamp.fromDate(now),
             closeTime: firebase.firestore.Timestamp.fromDate(
-                new Date(now.getTime() + (openTest.duration + 10 * 60) * 1000)
+                new Date(now.getTime() + (test.duration + 10 * 60) * 1000)
             ),
         });
+    };
+
+    const handleDeleteTest = test => async () => {
+        await testsRef.doc(test.id).delete();
     };
 
     const handleAddTest = wrapAction(async ({ name, type, duration, team, numPerSet }) => {
@@ -106,20 +142,7 @@ const TestsTab = () => {
     return (
         <Stack spacing={4}>
             {Object.values(testsById).map(x => (
-                <TestCard
-                    {...x}
-                    time={time}
-                    onOpen={() =>
-                        openDialog({
-                            type: "confirm",
-                            title: "Are you sure?",
-                            description: `This will open the test for all students for a window of ${
-                                x?.duration / 60 + 10
-                            } minutes. Confirm?`,
-                            onConfirm: handleOpenTest(x),
-                        })
-                    }
-                />
+                <TestCard {...x} time={time} onOpen={handleOpenTest(x)} onDelete={handleDeleteTest(x)} />
             ))}
             <Card as={Stack} spacing={4} p={4} maxW="md">
                 <Heading size="md">Add Test</Heading>
