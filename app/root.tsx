dfcd3b4b2d67f5a786be74067010acda2b725dd9
Oliver Ni <oliver.ni@gmail.com>
2022-08-27 13:16:17 -0700

    Add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "ContestDojo",
   viewport: "width=device-width,initial-scale=1",
+  "msapplication-TileColor": "#f40808",
+  "theme-color": "#ffffff",
 });
+
+export const links: LinksFunction = () => [
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#f40808" },
+];
 
 export default function App() {
   return (
