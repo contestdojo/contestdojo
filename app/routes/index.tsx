c27493e94ac26c60f8787371135c08b75ebf6e1e
Oliver Ni <oliver.ni@gmail.com>
2022-08-29 00:37:46 -0700

    Redirect index to admin page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,8 @@
 import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
-import type { User } from "~/utils/auth.server";
+import { redirect } from "@remix-run/node";
 import { requireSession } from "~/utils/auth.server";
 
-type LoaderData = {
-  user: User;
-};
-
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await requireSession(request);
-  return json<LoaderData>({ user });
+  await requireSession(request);
+  return redirect("/admin");
 };
-
-export default function IndexRoute() {
-  const { user } = useLoaderData<LoaderData>();
-
-  return (
-    <div>
-      <h1>Welcome to ContestDojo!</h1>
-      <p>Logged in as: {user.displayName}</p>
-
-      {user.isAdmin && (
-        <p>
-          <Link to="/admin">Go to Admin page</Link>
-        </p>
-      )}
-
-      <Form reloadDocument action="/logout" method="post">
-        <button type="submit">Sign out</button>
-      </Form>
-    </div>
-  );
-}
