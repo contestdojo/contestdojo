39248a63630826187ecc2a15612ea968b8ffaa6d
Oliver Ni <oliver.ni@gmail.com>
2022-08-30 15:44:59 -0700

    Fix dropdown align

--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -44,7 +44,9 @@ Dropdown.Items = function DropdownItems({
       leaveTo="transform opacity-0 scale-95"
     >
       <Menu.Items
-        className={clsx`absolute ${`${align}-0`} mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${className}`}
+        className={clsx`absolute ${
+          align === "left" ? "left-0" : "right-0"
+        } mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${className}`}
       >
         {children}
       </Menu.Items>
