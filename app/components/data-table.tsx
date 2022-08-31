df889c3995359b1ae7e122a8204c64cb799acaca
Oliver Ni <oliver.ni@gmail.com>
2022-08-30 22:26:51 -0700

    Refactor data table component

--- app/components/data-table.tsx
+++ app/components/data-table.tsx
@@ -6,12 +6,77 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import type { ColumnDef, RowData, TableState } from "@tanstack/react-table";
+import type { ColumnDef, RowData, Table, TableState } from "@tanstack/react-table";
 
 import { CheckIcon } from "@heroicons/react/20/solid";
 import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 
 import Dropdown from "./dropdown";
+
+type TableProps<T extends RowData> = { table: Table<T> };
+
+function FieldSelector<T extends RowData>({ table }: TableProps<T>) {
+  return (
+    <div className="sm:flex sm:justify-end">
+      <Dropdown>
+        <Dropdown.Button>Fields</Dropdown.Button>
+        <Dropdown.Items>
+          {table.getAllLeafColumns().map((x) => (
+            <Dropdown.Item
+              key={x.id}
+              className="group flex items-center"
+              onClick={x.getToggleVisibilityHandler()}
+            >
+              {x.getIsVisible() ? (
+                <CheckIcon className="-ml-1 mr-3 h-4 w-4" />
+              ) : (
+                <div className="-ml-1 mr-3 h-4 w-4" />
+              )}
+              {typeof x.columnDef.header === "string" ? x.columnDef.header : x.id}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Items>
+      </Dropdown>
+    </div>
+  );
+}
+
+function Headers<T extends RowData>({ table }: TableProps<T>) {
+  return (
+    <thead className="bg-gray-50">
+      {table.getHeaderGroups().map((headerGroup) => (
+        <tr key={headerGroup.id} className="divide-x divide-gray-200">
+          {headerGroup.headers.map((header) => (
+            <th
+              key={header.id}
+              className="whitespace-nowrap px-4 py-3 text-left text-sm font-semibold text-gray-900"
+              colSpan={header.colSpan}
+              scope="col"
+            >
+              {flexRender(header.column.columnDef.header, header.getContext())}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
+}
+
+function Body<T extends RowData>({ table }: TableProps<T>) {
+  return (
+    <tbody className="divide-y divide-gray-200 bg-white">
+      {table.getRowModel().rows.map((row) => (
+        <tr key={row.id} className="divide-x divide-gray-200">
+          {row.getVisibleCells().map((cell) => (
+            <td className="whitespace-nowrap px-4 py-2 text-sm text-gray-500" key={cell.id}>
+              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+}
 
 type DataTableProps<T extends RowData> = {
   data: T[];
@@ -33,74 +98,12 @@ export default function DataTable<T extends RowData>({
 
   return (
     <div className="flex flex-col items-stretch gap-4">
-      <div className="sm:flex sm:justify-end">
-        <Dropdown>
-          <Dropdown.Button>Fields</Dropdown.Button>
-          <Dropdown.Items>
-            {table.getAllLeafColumns().map((x) => (
-              <Dropdown.Item
-                key={x.id}
-                className="group flex items-center"
-                onClick={x.getToggleVisibilityHandler()}
-              >
-                {x.getIsVisible() ? (
-                  <CheckIcon className="-ml-1 mr-3 h-4 w-4" />
-                ) : (
-                  <div className="-ml-1 mr-3 h-4 w-4" />
-                )}
-                {typeof x.columnDef.header === "string" ? x.columnDef.header : x.id}
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Items>
-        </Dropdown>
-      </div>
+      <FieldSelector table={table} />
 
       <div className="overflow-auto rounded-lg shadow ring-1 ring-black ring-opacity-5">
         <table className="min-w-full divide-y divide-gray-300">
-          <thead className="bg-gray-50">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id} className="divide-x divide-gray-200">
-                {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    colSpan={header.colSpan}
-                    className="whitespace-nowrap px-4 py-3 text-left text-sm font-semibold text-gray-900"
-                    scope="col"
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id} className="divide-x divide-gray-200">
-                {row.getVisibleCells().map((cell) => (
-                  <td className="whitespace-nowrap px-4 py-2 text-sm text-gray-500" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-
-          {/* <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.footer, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot> */}
+          <Headers table={table} />
+          <Body table={table} />
         </table>
       </div>
     </div>
--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -32,6 +32,7 @@ Dropdown.Button = function DropdownButton({ className, children }: DropdownButto
       className={clsx`flex w-full items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 ${className}`}
     >
       {children}
+      <div className="grow" />
       <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
     </Menu.Button>
   );
