c7408402e6990c20caab26abb06daf01786616c9
Oliver Ni <oliver.ni@gmail.com>
2022-11-18 00:52:38 -0800

    Update confusing student home ui

--- pages/student/index.js
+++ pages/student/index.js
@@ -4,18 +4,17 @@
 
 import { Box, Button, Divider, Heading, HStack, Stack, Tooltip } from "@chakra-ui/react";
 import dayjs from "dayjs";
-import { useRouter } from "next/router";
-import { useFirestore, useFirestoreCollectionData } from "reactfire";
+import { useFirestore, useFirestoreCollection, useFirestoreCollectionData } from "reactfire";
 
 import ButtonLink from "~/components/ButtonLink";
 import Card from "~/components/Card";
 import { useUserData } from "~/helpers/utils";
 
-const EventCard = ({ id, name, studentRegistrationEnabled, date: { seconds } }) => {
-  const router = useRouter();
+const EventCard = ({ id, name, studentRegistrationEnabled, date: { seconds }, mine }) => {
   const date = dayjs.unix(seconds);
+
   return (
-    <Card as={HStack} p={4} maxWidth="sm">
+    <Card as={HStack} p={4}>
       <Box flex={1}>
         <Box as="h4" fontWeight="semibold" isTruncated>
           {name}
@@ -24,7 +23,11 @@ const EventCard = ({ id, name, studentRegistrationEnabled, date: { seconds } }) 
           {date.format("MMMM D, YYYY")}
         </Box>
       </Box>
-      {studentRegistrationEnabled ? (
+      {mine ? (
+        <ButtonLink href={`/student/${id}`} mt={2} colorScheme="blue" size="sm">
+          Go to Event
+        </ButtonLink>
+      ) : studentRegistrationEnabled ? (
         <ButtonLink href={`/student/${id}`} mt={2} colorScheme="blue" size="sm">
           Register
         </ButtonLink>
@@ -40,13 +43,21 @@ const EventCard = ({ id, name, studentRegistrationEnabled, date: { seconds } }) 
 };
 const Home = () => {
   const firestore = useFirestore();
-  const { data: user } = useUserData();
+  const { data: user, ref: userRef } = useUserData();
 
   // Get events
   const eventsRef = firestore.collection("events");
-  let { data: events } = useFirestoreCollectionData(eventsRef, { idField: "id" });
+  const { data: events } = useFirestoreCollectionData(eventsRef, { idField: "id" });
 
-  events = events.filter((x) => !x.hide);
+  // Get registered events
+
+  const studentsQuery = firestore.collectionGroup("students").where("user", "==", userRef);
+  const students = useFirestoreCollection(studentsQuery, { idField: "id" }).data.docs;
+  const myEventIds = students.map((x) => x.ref.parent.parent.id);
+
+  const myEvents = events.filter((x) => !x.hide && myEventIds.includes(x.id));
+  const otherEvents = events.filter((x) => !x.hide && !myEventIds.includes(x.id));
+  const pastEvents = events.filter((x) => x.hide && myEventIds.includes(x.id));
 
   return (
     <Stack spacing={6} maxW={600} mx="auto">
@@ -55,10 +66,21 @@ const Home = () => {
       </Heading>
       <Divider />
 
-      <Heading size="lg">Event Registration</Heading>
-      {events.map((x) => (
+      <Heading size="lg">My Events</Heading>
+      {myEvents.map((x) => (
+        <EventCard key={x.id} {...x} mine />
+      ))}
+
+      {otherEvents.length > 0 && <Heading size="lg">Events Accepting Registrations</Heading>}
+      {otherEvents.map((x) => (
         <EventCard key={x.id} {...x} />
       ))}
+
+      {pastEvents.length > 0 && <Heading size="lg">My Past Events</Heading>}
+      {pastEvents.map((x) => (
+        <EventCard key={x.id} {...x} mine />
+      ))}
+
       {events.length == 0 && <p>No events to register for at this time.</p>}
     </Stack>
   );
