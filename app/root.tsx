1f5f1d715b941462ae7b5ff3f7b48faa77c24771
Oliver Ni <oliver.ni@gmail.com>
2024-03-30 03:08:18 -0700

    Add error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -7,21 +7,28 @@
  */
 
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { PropsWithChildren } from "react";
 
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
   useNavigation,
+  useRouteError,
 } from "@remix-run/react";
+import clsx from "clsx";
 import nProgress from "nprogress";
 import { useEffect } from "react";
 
 import nProgressStyles from "~/nprogress.css";
 import styles from "~/tailwind.css";
+
+import { Alert, AlertStatus, Button } from "./components/ui";
 
 export const meta: MetaFunction = () => [
   { name: "charset", content: "utf-8" },
@@ -42,6 +49,56 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: nProgressStyles },
 ];
 
+type WrapperProps = PropsWithChildren<{
+  className?: string;
+}>;
+
+function Wrapper({ className, children }: WrapperProps) {
+  return (
+    <html lang="en" className="h-full bg-gray-100">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className={clsx`h-full ${className}`}>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  let title = "Error";
+  let description = "An unknown error occurred.";
+
+  if (isRouteErrorResponse(error)) {
+    title = `${error.status} ${error.statusText}`;
+    description = error.data;
+  } else if (error instanceof Error) {
+    title = error.name;
+    description = error.message;
+  }
+
+  return (
+    <Wrapper className="mx-auto flex max-w-2xl flex-col gap-4 p-6">
+      <Alert status={AlertStatus.Error} title={title}>
+        {description}
+      </Alert>
+      <div className="flex justify-center gap-4">
+        <Button onClick={() => window.location.reload()}>Reload</Button>
+        <Button as={Link} to="/">
+          Go Home
+        </Button>
+      </div>
+    </Wrapper>
+  );
+}
+
 export default function App() {
   const navigation = useNavigation();
 
@@ -54,17 +111,8 @@ export default function App() {
   }, [navigation.state]);
 
   return (
-    <html lang="en" className="h-full bg-gray-100">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
   );
 }
